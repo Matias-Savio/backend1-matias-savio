b4923cd0767685957c48d297dc03343bde740e0a
Matias <ezequielsavio@hotmail.com>
2026-01-31 00:52:33 -0300

    primer endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
-const http = require("http");
+import express from "express";
+const app = express();
+const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  res.end("Servidor Wed");
+const usuarios = [
+  {
+    id: 1,
+    nombre: "Juan",
+    email: "juan@ejemplo.com",
+  },
+  {
+    id: 2,
+    nombre: "María",
+    email: "maria@ejemplo.com",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("Hola desde Express");
 });
 
-server.liesten(8080, () => {
-  console.log(`Servidor Wed en el puerto ${8080}`);
+app.get("/bienvenido", (req, res) => {
+  res.send("Bienvenido usuario/a");
+});
+
+app.get("/usuario", (req, res) => {
+  const usuario = {
+    nombre: "Juan",
+    email: "juan@ejemplo.com",
+  };
+  res.json(usuario);
+});
+
+app.get("/usuario/:id", (req, res) => {
+  const id = req.params.id;
+  const user = usuarios.find((item) => item.id == id);
+  res.json(user);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor con Express en el puerto ${PORT}`);
 });
